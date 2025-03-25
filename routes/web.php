9a309305fa438f8c07cd221542dfec241a5cd980
WHISKY98 <mohammedmeski2002gmail.com>
2025-03-25 15:43:09 +0000

    changer the routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return inertia('Home');});
 Route::get('/login', function () {return inertia('Login');});
 Route::get('/register', function () {return inertia('Register');});
+
+
+
+
+Route::get('/users', function () {return inertia('users/index');});
+Route::get('/livres', function () {return inertia('livress/index');});
+Route::get('/users', function () {return inertia('users/index');});
+Route::get('/users', function () {return inertia('users/index');});
+
+
