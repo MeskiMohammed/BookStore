c00e52e3cb98d16095829d690672124c2dc594a7
WHISKY98 <mohammedmeski2002gmail.com>
2025-03-28 13:46:38 +0000

    added somethings

--- resources/js/Layout/admin/app.jsx
+++ resources/js/Layout/admin/app.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import NavBar from './navbar.jsx';
 import SideBar from './sidebar.jsx';
 import Footer from './footer.jsx';
@@ -7,9 +8,9 @@ export default function App(props) {
     <>
       <NavBar />
       <SideBar />
-      <div class='p-4 sm:ml-64 mt-14 overflow-y-auto h-[calc(100dvh-0.25rem*14)]'>
+      <div className='p-4 sm:ml-64 mt-14 overflow-y-auto h-[calc(100dvh-0.25rem*14)]'>
         <main>{props.children}</main>
-        <footer />
+        <Footer />
       </div>
     </>
   );
