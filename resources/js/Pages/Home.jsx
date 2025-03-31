5198ef840c47f1ff6792d5733390811c9ad03d5d
Zakariyae Ait Ali <116954709+ZakariaAit51@users.noreply.github.com>
2025-03-31 19:54:53 +0000

    `Update logo image and replace SVG icons with image tags in multiple files`

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -75,8 +75,7 @@ function Home() {
         <div className='grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12'>
           {/* Icon Block */}
           <div>
-          <svg xmlns="http://www.w3.org/2000/svg" className='shrink-0 size-9 text-gray-800 '  width='24' height='24' viewBox="0 0 512 512"><path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm161.5-86.1c-12.2-5.2-26.3 .4-31.5 12.6s.4 26.3 12.6 31.5l11.9 5.1c17.3 7.4 35.2 12.9 53.6 16.3l0 50.1c0 4.3-.7 8.6-2.1 12.6l-28.7 86.1c-4.2 12.6 2.6 26.2 15.2 30.4s26.2-2.6 30.4-15.2l24.4-73.2c1.3-3.8 4.8-6.4 8.8-6.4s7.6 2.6 8.8 6.4l24.4 73.2c4.2 12.6 17.8 19.4 30.4 15.2s19.4-17.8 15.2-30.4l-28.7-86.1c-1.4-4.1-2.1-8.3-2.1-12.6l0-50.1c18.4-3.5 36.3-8.9 53.6-16.3l11.9-5.1c12.2-5.2 17.8-19.3 12.6-31.5s-19.3-17.8-31.5-12.6L338.7 175c-26.1 11.2-54.2 17-82.7 17s-56.5-5.8-82.7-17l-11.9-5.1zM256 160a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
-
+        <svg class="shrink-0 size-9" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 10v12"/><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"/></svg>
             <div className='bg-linear-to-r from-blue-500 via-blue-50 to-transparent h-0.5 mt-6'>
               <div className='bg-blue-400 w-9 h-0.5'></div>
             </div>
