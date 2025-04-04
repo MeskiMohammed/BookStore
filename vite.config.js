322efd2d422beb2fd87c9d614daf91546e547aed
WHISKY98 <mohammedmeski2002gmail.com>
2025-04-04 15:00:15 +0000

    idk what i did

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,13 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
-  server: {
+    /*server: {
     host: '0.0.0.0', // Allow access from the network
     port: 5173, // Default Vite port
     strictPort: true,
+    cors: true,
     hmr: {
       host: '192.168.100.57', // Replace with your actual local IP
     },
-  },
+    },*/
 });
