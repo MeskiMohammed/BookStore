a020f80e6c19ec7b4bef60ab411894e956fbe66a
WHISKY98 <mohammedmeski2002gmail.com>
2025-04-08 18:52:53 +0100

    trying to commit if there is unsaved work

--- resources/js/Pages/store/cart.jsx
+++ resources/js/Pages/store/cart.jsx
@@ -1,4 +1,4 @@
-import StoreLayout from '@/Layouts/StoreLayout';
+import StoreLayout from '@/Layout/store/app';
 import CartPage from '@/components/store/cart-page';
 
 export default function App(props) {
