95ace45acfb31f8d37385aee37b916387e0e82d9
Mohammed Meski <mohammedmeski200@gmail.com>
2025-04-08 02:52:28 +0100

    fixed the profile dropdown in admin panel

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -34,11 +34,38 @@ export default function AdminLayout({ children }) {
               </Link>
               <div className='flex items-center ml-3'>
                 <div>
-                  <button type='button' className='flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300' id='user-menu-button'>
+                  <button type='button' className='flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300' id='user-menu-button' aria-expanded='false' data-dropdown-toggle='dropdown-user'>
                     <span className='sr-only'>Open user menu</span>
                     <img className='w-8 h-8 rounded-full' src='https://flowbite.com/docs/images/people/profile-picture-5.jpg' alt='user photo' />
                   </button>
                 </div>
+                <div className='z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm :bg-gray-700 ' id='dropdown-user'>
+                <div className='px-4 py-3' role='none'>
+                  <p className='text-sm text-gray-900 :text-white' role='none'>
+                    Neil Sims
+                  </p>
+                  <p className='text-sm font-medium text-gray-900 truncate :text-gray-300' role='none'>
+                    neil.sims@flowbite.com
+                  </p>
+                </div>
+                <ul className='py-1' role='none'>
+                  <li>
+                    <Link href='/admin' className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 :text-gray-300 ' role='menuitem'>
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href='/profile' className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 :text-gray-300 ' role='menuitem'>
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href='/signout' className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 :text-gray-300 ' role='menuitem'>
+                      Sign out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
               </div>
             </div>
           </div>
